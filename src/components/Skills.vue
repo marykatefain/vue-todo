4b5097b94216e42d3bdea4a894ff045179273948
Mary Kate Fain <marykate@promptworks.com>
2018-11-20 09:51:04 -0500

    forms and animations

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="container">
     <div class="holder">
+
+      <form @submit.prevent="addSkill">
+
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
+
+      </form>
+
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+        </transition-group>
       </ul>
       <p>These are the skills that you possess.</p>
     </div>
@@ -14,15 +27,23 @@
     name: 'Skills',
     data() {
       return {
+        skill: '',
         skills: [
           { "skill": "Vue.js" },
           { "skill": "Frontend Developer" }
         ],
-        alertObject: {
-           bgColor: 'yellow',
-           bgWidth: '100%',
-           bgHeight: '30px'
-        }
+      }
+    },
+    methods: {
+      addSkill() {
+        this.$validator.validateAll().then((result) => {
+          if (result) {
+            this.skills.push({skill: this.skill})
+            this.skill = '';
+          } else {
+            console.log("not valid");
+          }
+        })
       }
     }
   }
